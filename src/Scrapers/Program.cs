fe4295c0cf4ce2ce03483dfb1ea37415e9f8090d
Kaelan Fouwels <kfouwels@outlook.com>
2015-08-27 21:17:02 +0100

    scrape loop

--- src/Scrapers/Program.cs
+++ src/Scrapers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,24 @@ namespace Demo_scrapers
     {
         public void Main(string[] args)
         {
+			const int startId = 4499;
+			const int endId = 5499;
+
+			var n = endId - startId + 1;
+
+			var Tasks = new Task[n];
+
+			for (var index = startId; index <= endId; index++)
+			{
+				Tasks[index - startId] = Scrape(index);
+			}
+
+			Task.WaitAll(Tasks);
+			Console.ReadLine();
         }
+		public async Task Scrape(int Id)
+		{
+			Debug.WriteLine("scraping: " + Id.ToString());
+		}
     }
 }
