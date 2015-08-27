ae23fc7821bd7385e77eac300f7c909988b01eab
Kaelan Fouwels <kfouwels@outlook.com>
2015-08-27 21:27:02 +0100

    [scraper]  loading into hpack

--- src/Scrapers/Program.cs
+++ src/Scrapers/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using HtmlAgilityPack;
 
 namespace Demo_scrapers
 {
@@ -29,14 +30,24 @@ namespace Demo_scrapers
         }
 		public async Task Scrape(int Id)
 		{
-			Debug.WriteLine("scraping: " + Id.ToString());
+			//Debug.WriteLine("scraping: " + Id.ToString());
 
 			var Page = "";
 
 			using (var ht = new HttpClient())
 			{
-				Page = await (await ht.GetAsync(RootUrl + Id.ToString())).Content.ReadAsStringAsync();
+				var r = (await ht.GetAsync(RootUrl + Id.ToString()));
+				Debug.Write(r.StatusCode + " on page: " + Id.ToString());
+				if (r.StatusCode != System.Net.HttpStatusCode.OK) { Console.WriteLine(" <!>"); return; }
+				Console.WriteLine();
+
+                Page = await r.Content.ReadAsStringAsync();
 			}
+
+			var packdoc = new HtmlAgilityPack.HtmlDocument();
+			packdoc.Load(Page);
+
+
 
 		}
     }
